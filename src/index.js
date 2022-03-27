fb351332c1b4268a6af1cbaaa57e3fd341051b76
Manyu Duan <z5259564@unsw.edu.au>
2022-03-28 00:48:09 +1100

    poopy

--- src/index.js
+++ src/index.js
@@ -4,91 +4,105 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import styled from 'styled-components';
 
-const categoriesArray = ["Weeb","Emotional Damage","cat3","cat4","cat5"];
+const categoriesArray = ["Weeb", "Emotional Damage", "cat3", "cat4", "cat5"];
 const questionsArray = [
-  [{q:"The 3rd opening is Blue Bird by Ikimono Gakari",a:"What is a Naruto Shippuden Opening",i:"cute.jpg"},
-    {q:"This anime series is based on the premise of summoning historical figures for a battle royale.",a:" What is Fate",i:"",ia:""},
-    {q:"The fifth isekai series to join Isekai Quartet",a:" What is The Rising of the Shield Hero/Tate no Yuusha no Nariagari",i:"",ia:""},
-    {q:"This anime is infamous for repeating the same episode with minor differences eight times in a row",a:"What is The Melancholy of Haruhi/Suzumiya Haruhi no Yuutsu",i:"",ia:""},
-    {q:"Hayao Miyazaki's directorial debut",a:"What is Lupin III: The Castle of Cagliostro/Lupin III: Cagliostro no Shiro",i:"",ia:""},],
-  [{q:"help",a:"me",i:"", ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
+    [{ q: "The 3rd opening is Blue Bird by Ikimono Gakari", a: "What is a Naruto Shippuden Opening", i: "cute.jpg" },
+        { q: "This anime series is based on the premise of summoning historical figures for a battle royale.", a: " What is Fate", i: "", ia: "" },
+        { q: "The fifth isekai series to join Isekai Quartet", a: " What is The Rising of the Shield Hero/Tate no Yuusha no Nariagari", i: "", ia: "" },
+        { q: "This anime is infamous for repeating the same episode with minor differences eight times in a row", a: "What is The Melancholy of Haruhi/Suzumiya Haruhi no Yuutsu", i: "", ia: "" },
+        { q: "Hayao Miyazaki's directorial debut", a: "What is Lupin III: The Castle of Cagliostro/Lupin III: Cagliostro no Shiro", i: "", ia: "" },
+    ],
+    [{ q: "help", a: "me", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
 ];
-const categoriesArray2 = ["0w0","cat2","cat3","cat4","cat5"];
+const categoriesArray2 = ["0w0", "cat2", "cat3", "cat4", "cat5"];
 const questionsArray2 = [
-  [{q:"THIS IS QUESTION SET 2",a:"answer: UR MOM",i:"cute.jpg"},
-    {q:"help",a:"me",i:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
-  [{q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},
-    {q:"",a:"",i:"",ia:""},],
+    [{ q: "THIS IS QUESTION SET 2", a: "answer: UR MOM", i: "cute.jpg" },
+        { q: "help", a: "me", i: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
+    [{ q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+        { q: "", a: "", i: "", ia: "" },
+    ],
 ];
 
 
-const ContentWrap = styled.div`
-  background-color: #999999;
-  border: solid gray 5px;
+const ContentWrap = styled.div `
+  background-color: #e7b1ffff;
   position:absolute;
+  border: 5px solid #a4f6ffff;
   top: 5vw;
   left:15vw;
   right:15vw;
   padding:20px 50px;
+  box-shadow: 20px 20px aqua;
+  glow:
   z-index:99;
 `
-const Button = styled.div`
+const Button = styled.div `
   user-select: none;
   -moz-user-select: none;
   -webkit-user-select: none;
   -ms-user-select: none;
   cursor: pointer;
 `
-const ExitButton = styled(Button)`
+const ExitButton = styled(Button)
+`
   position:absolute;
-  top:0;
-  right:0;
-  background-color:red;
+  top:0px;
+  right:0px;
+  background-color: #FD3A2A;
   color:black;
   width:50px;
   height:50px;
 `
-const ShowAnswer = styled(Button)`
+const ShowAnswer = styled(Button)
+`
   background-color:white;
   color:black;
   width:250px;
@@ -96,19 +110,20 @@ const ShowAnswer = styled(Button)`
   margin-left:auto;
   margin-right:auto;
 `
-const Image = styled.img`
+const Image = styled.img `
   width:400px;
   height:400px;
 `
-const GenericWrap = styled.div`
+const GenericWrap = styled.div `
   text-align: center;
   color:black;
   padding:10px;
 `
-const Header = styled(GenericWrap)`
+const Header = styled(GenericWrap)
+`
   font-size:2rem;
 `
-const Arrow = styled.div`
+const Arrow = styled.div `
   border: solid black;
   border-width: 0 3px 3px 0;
   display: inline-block;
@@ -117,157 +132,204 @@ const Arrow = styled.div`
   transition:0.2s;
   margin:3px;
   `
+
 function ShowQuestion(props) {
 
-  return (
-    <ContentWrap>
-        <ExitButton onClick = {props.closeOnClick}>x</ExitButton>
-        <Header>{props.question.q}</Header>
-        <br/>
-        {props.question.i !== "" ? <Image src = {props.question.i} alt = ""/>
-        :''
-        }
-      <ShowAnswer onClick = {props.SetAnswer}><Arrow state = {props.showanswer}/>Answer</ShowAnswer>  
-      <GenericWrap>{props.showanswer ? props.question.a : ""}</GenericWrap>
-    </ContentWrap>
-  )
+    return ( <
+        ContentWrap >
+        <
+        ExitButton onClick = { props.closeOnClick } > x < /ExitButton> <
+        Header > { props.question.q } < /Header> <
+        br / > {
+            props.question.i !== "" ? < Image src = { props.question.i }
+            alt = "" / >
+            :
+                ''
+        } <
+        ShowAnswer onClick = { props.SetAnswer } > < Arrow state = { props.showanswer }
+        />Answer</ShowAnswer >
+        <
+        GenericWrap > { props.showanswer ? props.question.a : "" } < /GenericWrap> < /
+        ContentWrap >
+    )
 }
-const Cell = styled.th`
+const Cell = styled.th `
   margin:5px;
   width:300px;
   height: 80px;
   margin:20px;
 `
-const BoxWrap = styled(Cell)`
+const BoxWrap = styled(Cell)
+`
   color: ${props=>props.state ? "yellow":"black"};
-  background-color: ${props=>
-      props.state === false && "#2222bb" ||
-      props.hover === true && "#4444ff" ||
-      "#3333dd"
-  };
-  background: linear-gradient(
-  45deg,
+  background: linear-gradient( 45deg,
   ${props=>
-      props.state === false && "#2222bb" ||
-      props.hover === true && "#4444ff" ||
-      "#3333dd"
+      props.state === false && "#b1b2ffff" ||
+      props.hover === true && "#ffb1e0ff" ||
+      "#B361C0"
   },
   #6c79e9);
   /* #6c79e9 40px,
   ${props=>
       props.state === false && "#2222bb" ||
-      props.hover === true && "#4444ff" ||
-      "#3333dd"
+      props.hover === true && "#ffb1e0ff" ||
+      "#B361C0"
   } 50px */
 
 `
-
-const CatWrap = styled(Cell)`
-  background-color: #4444ff;
+const CatWrap = styled(Cell)
+`
+  background-color: #ffb1e0ff ;
   color:white;
-  text-shadow:2px 2px black;
+  text-shadow:2px 2px 5px #ca007aff;
   font-size: 1.5rem;
 `
-const FullDiv = styled.div`
+const FullDiv = styled.div `
   padding-top: 25px;
   height:100%;
 `
-function Box(props) {
-  const [state, setState] = useState(true);
-  const [visible, setVisible] = useState(false);
-  const [answer, SetAnswer] = useState(false);
-  const [hover, SetHover] = useState(false);
-  return (
-    <BoxWrap state = {state} hover = {hover}>
-      <FullDiv onClick = {() => { setVisible(true)}} onMouseEnter = {()=>SetHover(true)} onMouseLeave = {()=>SetHover(false)}> 
-        {props.value}
-      </FullDiv>
-      {visible ? <ShowQuestion question = {props.question} visible = {visible} closeOnClick = {() => {setVisible(false); setState(false)}} SetAnswer = {() => SetAnswer(!answer)}
-      showanswer = {answer} >
 
-      </ShowQuestion> : ''}
-      
-    </BoxWrap>
-  ) 
+function Box(props) {
+    const [state, setState] = useState(true);
+    const [visible, setVisible] = useState(false);
+    const [answer, SetAnswer] = useState(false);
+    const [hover, SetHover] = useState(false);
+    return ( <
+        BoxWrap state = { state }
+        hover = { hover } >
+        <
+        FullDiv onClick = {
+            () => { setVisible(true) }
+        }
+        onMouseEnter = {
+            () => SetHover(true)
+        }
+        onMouseLeave = {
+            () => SetHover(false)
+        } > { props.value } <
+        /FullDiv> {
+        visible ? < ShowQuestion question = { props.question }
+        visible = { visible }
+        closeOnClick = {
+            () => {
+                setVisible(false);
+                setState(false)
+            }
+        }
+        SetAnswer = {
+            () => SetAnswer(!answer)
+        }
+        showanswer = { answer } >
+
+        <
+        /ShowQuestion> : ''}
+
+        <
+        /BoxWrap>
+    )
 }
-const Table = styled.table`
+const Table = styled.table `
   background-color:black;
   margin-left:auto;
   margin-right:auto;
+  border: 1px, solid, rgba(0,0,0,1);
   width:100%;
 `
+
 function MoneyGrid(props) {
- // const array = new Array(5).fill(new Array(5).fill(0));
-  //  ((index / 5) + 1)*200 
-  return (
-    
-      <Table>
-      <tr>
-        {props.cats.map( (element, col) => (
-            <CatWrap>{element}</CatWrap>
-          ))}
-      </tr>
-      {props.qs.map( (row, rowIndex, mainArray) => (
-          <tr>
-            {row.map( (element, colIndex) => (
-              <Box value = {`$${( rowIndex + 1) * 100 * props.factor}`} question = {mainArray.at(colIndex).at(rowIndex)}> </Box>
-            ))}
-          </tr>
-        ))}
-      </Table>
-  )
+    // const array = new Array(5).fill(new Array(5).fill(0));
+    //  ((index / 5) + 1)*200 
+    return (
+
+            <
+            Table >
+            <
+            tr > {
+                props.cats.map((element, col) => ( <
+                    CatWrap > { element } < /CatWrap>
+                ))
+            } <
+            /tr> {
+            props.qs.map((row, rowIndex, mainArray) => ( <
+                tr > {
+                    row.map((element, colIndex) => ( <
+                        Box value = { `$${( rowIndex + 1) * 100 * props.factor}` }
+                        question = { mainArray.at(colIndex).at(rowIndex) } > < /Box>
+                    ))
+                } <
+                /tr>
+            ))
+        } <
+        /Table>
+)
 }
 
 
-const PageWrap = styled.div`
+const PageWrap = styled.div `
   width:100vw;
   height:100vh;
   margin:0;
   top:0;
   position:absolute;
 `
-const NavButtonWrapper = styled.div`
+const NavButtonWrapper = styled.div `
   position:absolute;
+  align:center;
+  padding-right: 15px;
+  padding-top: 5px;
   bottom:0;
-  background-color:#1111ee;
+  background-color: #AB8DD8;
 `
-const NavButton = styled(Button)`
+const NavButton = styled(Button)
+`
   height:40px;
-  padding:10px;
+  align:center;
+  padding:15px;
   display:inline-block;
   &:hover {
-    background-color:#2222ff;
+    background-color: #B06BC5;
   }
 `
-const H1 = styled.div`
+const H1 = styled.div `
   font-size:3rem;
+  font-family: 'Pacifico', cursive;
   text-align:center;
+  text-shadow:2px 2px 5px #ffb1e0ff;
   padding:5px;
 `
-function Page (props) {
-  const [currPage, setPage] = useState(0);
-  const highestPage = 1;
-  const cats = [categoriesArray, categoriesArray2];
-  const qs = [questionsArray, questionsArray2];
-  const factors = [1,2];
-  return(
-  <PageWrap>
-    <H1>Aibou Arena</H1>
-    <MoneyGrid cats = {cats.at(currPage) } qs = {qs.at(currPage)} factor = {factors.at(currPage)}/>
-    <br/>
-    <NavButtonWrapper>
-      <NavButton onClick={ () => setPage(Math.max(currPage - 1,0))}>previous</NavButton>
-      <NavButton onClick={ () => setPage(Math.min(currPage + 1,highestPage))}>forward</NavButton>
-      page: {currPage}
-    </NavButtonWrapper>
-  </PageWrap>
-  )
+
+function Page(props) {
+    const [currPage, setPage] = useState(0);
+    const highestPage = 1;
+    const cats = [categoriesArray, categoriesArray2];
+    const qs = [questionsArray, questionsArray2];
+    const factors = [1, 2];
+    return ( <
+        PageWrap >
+        <
+        H1 > Aibou Arena < /H1> <
+        MoneyGrid cats = { cats.at(currPage) }
+        qs = { qs.at(currPage) }
+        factor = { factors.at(currPage) }
+        /> <
+        br / >
+        <
+        NavButtonWrapper >
+        <
+        NavButton onClick = {
+            () => setPage(Math.max(currPage - 1, 0))
+        } > previous < /NavButton> <
+        NavButton onClick = {
+            () => setPage(Math.min(currPage + 1, highestPage))
+        } > forward < /NavButton>
+        page: { currPage } <
+        /NavButtonWrapper> < /
+        PageWrap >
+    )
 }
 
 
-
-
-ReactDOM.render(
-  <Page/>,
-  document.getElementById('root')
+ReactDOM.render( <
+    Page / > ,
+    document.getElementById('root')
 );
